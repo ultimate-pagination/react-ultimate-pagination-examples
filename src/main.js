088331a7e2159450d95da7a366cbf9b4f61f1692
Dmytro Yarmak <dmytroy@backbase.com>
2016-04-19 23:34:08 +0300

    Update example to use version 0.2.0

--- src/main.js
+++ src/main.js
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {UltimatePagination} from 'react-ultimate-pagination';
 
+const styles = {
+  label: {
+    display: 'block'
+  },
+  input: {
+    width: 100
+  }
+};
+
+
 class App extends React.Component {
   constructor() {
     super();
 
     this.onTotalPagesChange = this.onTotalPagesChange.bind(this);
     this.onCurrentPageChange = this.onCurrentPageChange.bind(this);
+    this.onPageChangeFromPagination = this.onPageChangeFromPagination.bind(this);
 
     this.state = {
       currentPage: 1,
@@ -23,34 +34,56 @@ class App extends React.Component {
     this.setState({currentPage: +event.target.value});
   }
 
+  onPageChangeFromPagination(newPage) {
+    this.setState({currentPage: newPage});
+  }
+
   render() {
     return (
       <div>
-        <div>
-          <label htmlFor="totalPages">Total pages:</label>
-          <input
-            type="number"
-            id="totalPages"
-            value={this.state.totalPages}
-            min="1"
-            onChange={this.onTotalPagesChange}
+        <h1>Ultimate Pagination</h1>
+        <fieldset>
+          <legend>Component configuration</legend>
+          <div>
+            <label htmlFor="totalPages" style={styles.label}>
+              Total pages:
+            </label>
+            <input
+              type="number"
+              id="totalPages"
+              style={styles.input}
+              value={this.state.totalPages}
+              min="1"
+              onChange={this.onTotalPagesChange}
+            />
+          </div>
+
+
+          <div>
+            <label htmlFor="currentPage" style={styles.label}>
+              Current page:
+            </label>
+            <input
+              type="number"
+              id="currentPage"
+              style={styles.input}
+              value={this.state.currentPage}
+              min="1"
+              max={this.state.totalPages}
+              onChange={this.onCurrentPageChange}
+            />
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>Live example</legend>
+
+          <UltimatePagination
+            currentPage={this.state.currentPage}
+            totalPages={this.state.totalPages}
+            onChange={this.onPageChangeFromPagination}
           />
-        </div>
-
-
-        <div>
-          <label htmlFor="currentPage">Current page:</label>
-          <input
-            type="number"
-            id="currentPage"
-            value={this.state.currentPage}
-            min="1"
-            max={this.state.totalPages}
-            onChange={this.onCurrentPageChange}
-          />
-        </div>
-
-        <UltimatePagination currentPage={this.state.currentPage} totalPages={this.state.totalPages}/>
+        </fieldset>
       </div>
     );
   }
